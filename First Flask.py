9222c737205918fa0fbcd6d16dc9c204670e74cb
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-08-15 14:49:03 +0530

    Commit 2 Learnt url  Redirection

--- First Flask.py
+++ First Flask.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from time import sleep
+from flask import Flask, redirect, url_for
 import pyautogui as pg
 import webbrowser as wb
 
@@ -7,8 +8,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello! This is the main page <h1>HELLO</h1>"
+wb.open("http://127.0.0.1:5000")
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello! You are in the page of {name}!"
+print("Done!")
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 if __name__ == "__main__":
     app.run()
-    wb.open("http://127.0.0.1:5000")
-    pg.moveTo(700, 800, duration=10)
