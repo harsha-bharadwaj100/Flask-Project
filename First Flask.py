a8bdc60c6e0bb54c56ce5da45bee0b167419172e
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-08-15 20:48:17 +0530

    Templates added!

--- First Flask.py
+++ First Flask.py
@@ -1,22 +1,28 @@
 from time import sleep
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 import pyautogui as pg
 import webbrowser as wb
 
 app = Flask(__name__)
 
+@app.route("/home")
 @app.route("/")
 def home():
-    return "Hello! This is the main page <h1>HELLO</h1>"
-wb.open("http://127.0.0.1:5000")
+    return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello! You are in the page of {name}!"
+# @app.route("/<name>")
+# def user(name, k):
+#     return render_template("user.html", content=name)
 print("Done!")
 
+@app.route("/Damn it's him/hrthjtr")
+def specialUser():
+    return render_template("user.html", content="Holy Admin!")
+    
 @app.route("/admin")
 def admin():
-    return redirect(url_for("home"))
+    return redirect(url_for("specialUser"))
+
 if __name__ == "__main__":
-    app.run()
+    # wb.open("http://127.0.0.1:5000")
+    app.run(debug=True)
